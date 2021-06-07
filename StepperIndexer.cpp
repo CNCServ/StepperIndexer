ee2a658d18b53e02b7028d456ff68c58a21d6aff
CNCServ <contact@Multicn.tech>
2021-06-07 18:41:31 +0200

    test

---
+++ StepperIndexer.cpp
@@ -0,0 +1,197 @@
+#include <Arduino.h>
+#include "StepperIndexer.h"
+
+int HardWareInit = 0;    // indique que le timer a bien été initialisé
+TStepperIndexer *StepperInstance[5];
+int NbInstance = 0;  
+int32_t cptmes=0;
+int32_t Speed_Max=12000;
+TStepperIndexer::TStepperIndexer(int8_t pin_Step, int8_t pin_Dir) // constructeur
+{
+   
+    StepperInstance[ NbInstance] = this;
+     NbInstance++;
+  
+    _Speed=0;
+    CptInterpolate=0;
+    _pin_Step = 1<<(pin_Step-8);
+    _pin_Dir = 1<<(pin_Dir-8);
+     pinMode(pin_Dir, OUTPUT); 
+     pinMode(pin_Step, OUTPUT); 
+     
+    
+    
+}
+int32_t TStepperIndexer::GetTest() 
+{
+  return  cptmes;
+}
+void TStepperIndexer::GoToTarget(int32_t Target,uint16_t Speed)
+{
+   
+    _Target = Target;
+    
+    if (Target == Position )
+      Speed = 0;
+    if (Target <  Position )
+      Speed = -Speed;
+    ModeTarget = 1;
+    SetSpeed(Speed) ;
+}
+void TStepperIndexer::SetSpeed(int16_t Speed) 
+{
+  
+   if ( Speed > Speed_Max)
+         Speed = Speed_Max;
+   if ( Speed < -Speed_Max)
+         Speed = -Speed_Max;      
+    _Speed = Speed * Coef_Speed;
+    if (Speed == 0)
+       ModeTarget = 0;
+}
+void TStepperIndexer::SetAccel(uint16_t Accel) 
+{
+    Speed_Max = FREQ_MAX / NbInstance;
+   Coef_Speed= (float)VAL_CPTMAX/Speed_Max;
+    _Accel = (Accel*Coef_Speed)/Speed_Max;
+    FAccel_div2 =  ((float)Accel*2);
+    FAccel_div2/= Speed_Max;
+    FAccel_div2/= Speed_Max;
+}
+void TStepperIndexer::Interrupt() 
+{
+  PORTB &=  0xff^_pin_Step; 
+  if( ModeTarget )
+       {
+          if (_Target== Position)
+          {
+             
+                SpeedInterpolate = 0; 
+              _Speed = 0; 
+              ModeTarget = 0;
+          }
+       }
+  if (! Stade )
+  {
+       if (Pulse ==2 )
+       {
+           PORTB |=  _pin_Step;
+            Pulse =0;
+        }
+       if ( SpeedInterpolate < _Speed )  {
+          SpeedInterpolate += _Accel;
+            if ( SpeedInterpolate > _Speed )
+              SpeedInterpolate = _Speed; 
+       }
+       else if (( SpeedInterpolate > _Speed ))   {
+          SpeedInterpolate -= _Accel;
+            if ( SpeedInterpolate < _Speed )
+              SpeedInterpolate = _Speed; 
+       }
+       CptInterpolate += SpeedInterpolate;
+       Stade =1;
+       Direction=(( CptInterpolate > 0 ) ? 1 : -1);
+       if (Direction == 1)
+          PORTB &=  0xff^_pin_Dir;
+       else
+          PORTB |=  _pin_Dir;
+          
+       
+       if ( CptInterpolate > VAL_CPTMAX)
+       {
+            CptInterpolate -= VAL_CPTMAX;
+            
+            Position++;
+             if ( CptInterpolate > (VAL_CPTMAX/2))
+                Pulse =  1;
+            else
+                Pulse =  2;
+                
+       }
+       if ( CptInterpolate < -VAL_CPTMAX )
+       {
+            CptInterpolate += VAL_CPTMAX;
+             if ( CptInterpolate < -(VAL_CPTMAX/2))
+                Pulse =  1;
+            else
+                Pulse =  2;
+            Position--;
+       }
+       
+        FSpeed = (float )  (SpeedInterpolate) /VAL_CPTMAX; 
+        //FSpeed /= Coef_Speed;
+        FSpeed *=  FSpeed;
+       
+   }
+   else
+    {
+       Stade =0;
+       if (Pulse ==1 )
+       {
+           PORTB |=  _pin_Step;
+            Pulse =0;
+        }
+       if ( ModeTarget ) 
+       {
+           if  (_Target >  Position) 
+           {
+             VMax = (_Target -  Position)  *  FAccel_div2;   
+           }
+           else
+           {
+             VMax = (Position - _Target)  *  FAccel_div2;
+           }
+         
+           if ( VMax < FSpeed )
+           {
+             if (_Speed > 0)
+             {
+                 if (_Speed > SpeedInterpolate)
+                  _Speed = SpeedInterpolate;
+                 _Speed -= _Accel;
+                 
+              }
+             else 
+             {
+                if (_Speed < SpeedInterpolate)
+                   _Speed = SpeedInterpolate;
+                _Speed += _Accel;
+              }
+           }
+        }
+       
+         
+      
+    }
+    
+}
+
+void StepperSetupHardWare()
+{
+  /* DDRD = B11111100; 
+     PORTD = 0;
+     DDRB = B00110001;
+     PORTB = B00001110;
+    DDRC = B00001000;
+    PORTC = B00110111; */  
+  pinMode(7, OUTPUT); // pour mesurer les durée d'interruption
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  OCR1A = 2000000 / (FREQ_MAX*2) ;            // compare match register
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS11);    // 8 prescaler
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts      
+}
+
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+  //PORTB = B0000001;
+  cptmes++;
+  StepperInstance[0]->Interrupt();
+//  PORTB = B0000000;
+
+}      
---
+++ StepperIndexer.h
@@ -0,0 +1,48 @@
+#ifndef StepperIndexer_h
+#define StepperIndexer_h
+//*********************************************
+//        Indexeur pour Moteur Pas à Pas
+//*********************************************
+//  version: V1.0
+// Auteur: Christian TROUILLET CNCServ    
+//Librairie permettant de piloter un ou plusieurs moteurs Pas à Pas en position et/ou en vitesse.
+// - Prise en compte des accélérations et de décellations en temps réel.
+// - fonction non bloquante
+#include <Arduino.h>
+#define FREQ_MAX 12000  //frequence maximum 12 kHZ pour un moteur
+#define VAL_CPTMAX 100000000 // valeur du compteur pour faire un pas
+void StepperSetupHardWare();     //initialisation de l'interruption timer
+class TStepperIndexer
+{
+  public:
+    int32_t Position;    // position du moteur, peut être lu et écrit.
+    
+    TStepperIndexer(int8_t pin_Step, int8_t pin_Dir);
+    int32_t GetTest();
+    void GoToTarget(int32_t Target,uint16_t Speed);
+    void SetSpeed(int16_t Speed);
+    void SetAccel(uint16_t Accel);
+    void Interrupt();
+ // private:
+    uint32_t _Accel;       // acceleration par cycle 
+    int8_t Stade;    // il y a 2 étapes dans l'interruption, une pour generer le pulse en sortie et une pour le remettre a zéro.
+                      // les calcul sont réparti sur les 2 étapes
+    int8_t ModeTarget;  // Indique qu'il faut prendre en compte la position cible
+    int8_t b_Deccel;     // a dévelloper
+    int32_t _Speed;       // vitesse de consigne = vitesse en Step/s X 
+    int32_t _Target;     // position cible
+    int32_t SpeedInterpolate;  // vitesse instantanée
+    int32_t CptInterpolate;  // quand ce compteur atteint  VAL_CPTMAX on fait un Step
+    int Direction;
+    int Pulse;                //à 1 il faut generer un pulse
+    
+    float Coef_Speed;        // coeficient pour convertir la vitesse de consigne 
+    float FSpeed;            // vitesse en flottant en step par cycle, donc toujours <= à 1 , elle est ensuite élevée au ²
+    float FAccel_div2;       // = 0.5a
+    float VMax;              // vitesse maximum au ² calculée en fonction de la distance restante -> D * 0.5 * a
+    int8_t _pin_Dir;           // pin de pour la sortie Dir entre 8 et 13
+    
+    int8_t _pin_Step;          // pin de pour la sortie Step entre 8 et 13
+};    
+    
+#endif
